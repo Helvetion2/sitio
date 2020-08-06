2945cfb439561c427b806e476c2ee95107293ccb
Helvetion2 <59890737+Helvetion2@users.noreply.github.com>
2020-08-06 20:31:20 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ myHeading.textContent = 'Hello world!';
 let miImage = document.querySelector('img');
 miImage.onclick = function () {
     let miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/firefox-icon.png') {
+    if (miSrc === 'images/giphy.gif') {
       miImage.setAttribute('src','images/firefox2.png');
     } else {
       miImage.setAttribute('src', 'images/firefox-icon.png');
