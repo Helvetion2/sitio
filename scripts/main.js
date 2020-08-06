63755c028c884a9efe9adefd0f4e34ccf4242836
Helvetion2 <59890737+Helvetion2@users.noreply.github.com>
2020-08-06 20:28:33 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/firefox-icon.png') {
+      miImage.setAttribute('src','images/firefox2.png');
+    } else {
+      miImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
