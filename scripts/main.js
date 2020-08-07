c93cb384b93df47136e43f60859ac2e0f18089bb
Helvetion2 <59890737+Helvetion2@users.noreply.github.com>
2020-08-06 22:14:08 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'TE GUSTAN LAS ACEITUNAS??';
 
-let miImage = document.querySelector('img');
-miImage.onclick = function () {
-let miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/sipp.png') {
-    alert('dios te bendiga, sos una buena preona');
-} else {
-      alert('zarpdado mogolico');
-       }
+let tanga = document.querySelector('imagenes/NO.jpg');
+tanga.onclick = function () {
+    alert ("Como toda persona de bien, te felicito");
 }
+    
+ let tanga2 = document.querySelector('imagenes/NO.jpg');
+tanga.onclick = function () {
+    alert ("Zarpada enfermedad mental sufris");
+}   
+    
