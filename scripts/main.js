6eb88b06838b952b9a919111a262a076c365511f
Helvetion2 <59890737+Helvetion2@users.noreply.github.com>
2020-08-06 22:25:31 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,11 @@ myHeading.textContent = 'TE GUSTAN LAS ACEITUNAS??';
 
 let tanga = document.getElementById('a');
 tanga.onclick = function () {
-    alert ("Como toda persona de bien, te felicito");
+    alert ("Como toda persona de bien te felicito");
 }
     
  let tanga2 = document.getElementById('b');
-tanga.onclick = function () {
+tanga2.onclick = function () {
     alert ("Zarpada enfermedad mental sufris");
 }   
     
