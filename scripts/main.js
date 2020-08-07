5b4cdf146b872fcd60caf046a3336c154f11e43c
Helvetion2 <59890737+Helvetion2@users.noreply.github.com>
2020-08-06 21:55:50 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'TE GUSTAN LAS ACEITUNAS??';
+
+document.querySelector('img').onclick = function() {
+
+let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/sipp.png') {
+    alert('Sos un tipazo');
+}else {
+      alert('zarpdado mogolico');
+    }
+}
+
+
