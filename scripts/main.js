2d8d3aade390776c89193873db1f867e55e11f41
Helvetion2 <59890737+Helvetion2@users.noreply.github.com>
2020-08-06 22:24:33 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'TE GUSTAN LAS ACEITUNAS??';
 
-let tanga = document.getElementById('1');
+let tanga = document.getElementById('a');
 tanga.onclick = function () {
     alert ("Como toda persona de bien, te felicito");
 }
     
- let tanga2 = document.getElementById('2');
+ let tanga2 = document.getElementById('b');
 tanga.onclick = function () {
     alert ("Zarpada enfermedad mental sufris");
 }   
