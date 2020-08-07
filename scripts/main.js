e953e1c4c3e507ded24d4f37f1a15af51070a353
Helvetion2 <59890737+Helvetion2@users.noreply.github.com>
2020-08-06 22:08:52 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,20 +6,7 @@ miImage.onclick = function () {
 let miSrc = miImage.getAttribute('src');
     if (miSrc === 'images/sipp.png') {
     alert('dios te bendiga, sos una buena preona');
-}else {
+} else {
       alert('zarpdado mogolico');
-    }
+       }
 }
-
-let miImage = document.querySelector('img');
-miImage.onclick = function () {
-    
-{
-let miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/NO.jpg') {
-    alert('zarpado mogolik');
-}else {
-      alert('zarpdado mogolico');
-    }
-}
-
